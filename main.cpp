9d56934791a83ed108bc7450dc4e09b410685671
kishitashiho <sc111016@ibe.kagoshima-u.ac.jp>
2014-03-30 17:31:21 +0900

    練習２－１
    先輩のを参考に書き直しました。
    ARDroneはちゃんと飛びました。

--- main.cpp
+++ main.cpp
@@ -6,27 +6,45 @@
 // Return value : SUCCESS:0  ERROR:-1
 // --------------------------------------------------------------------------
 
-
 ARDrone ardrone;
 
-void roll(int move_count);
-void forward(int move_count);
-void stop(int move_count);
+void roll(int move);
+void forward(int move);
+void stop(int move);
+void init();
+bool emergency();
 
 double vx = 0.0, vy = 0.0, vz = 0.0, vr = 0.0;
 
-//Ç±Ç±ÇÕÉÅÉCÉìÇ≈Ç∑
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
 
-	// Initialize
 	if (!ardrone.open()) {
 		printf("Failed to initialize.\n");
 		return -1;
 	}
 
+	init();
+
+	ardrone.takeoff();
+
+	stop(50);
+	forward(100);
+	stop(50);
+	roll(70);
+	stop(50);
+	forward(100);
+	stop(50);
+
+	ardrone.landing();
+
+	// See you
+	ardrone.close();
+
+	return 0;
+}
+
+void init() {
 	// Battery
 	printf("Battery = %d%%\n", ardrone.getBatteryPercentage());
 
@@ -50,106 +68,113 @@ int main(int argc, char **argv)
 	printf("*    'Esc'   -- Exit                  *\n");
 	printf("*                                     *\n");
 	printf("***************************************\n\n");
+}
 
-	ardrone.takeoff();
 
-	int count=0;
+void roll(int move){
+
+	int move_count = 0;
 
 	while (1) {
-		// Key input
-		int key = cvWaitKey(33);
-		if (key == 0x1b) break;
 
-		// Update
-		if (!ardrone.update()) break;
+		vx = 0.0;
+		vy = 0.0;
+		vz = 0.0;
+		vr = 1.0;
 
-		// Get an image
-		IplImage *image = ardrone.getImage();
-
-		// Take off / Landing 
-		if (key == ' ') {
-			if (ardrone.onGround()) ardrone.takeoff();
-			else                    ardrone.landing();
-		}
-
-		// Move
-
-		if (key == 0x260000) vx =  1.0;
-		if (key == 0x280000) vx = -1.0;
-		if (key == 0x250000) vr =  1.0;
-		if (key == 0x270000) vr = -1.0;
-		if (key == 'q')      vz =  1.0;
-		if (key == 'a')      vz = -1.0;
-
-		if(150<count && count<=200) {
-			forward(50);
-		}
-
-		if(200<count && count<=300) {
-			stop(10);
-		}
-
-		if(300<count && count<=400) {
-			roll(10);
-		}
-
-		if(400<count && count<=500) {
-			stop(10);
-		}
-
-		if(500<count && count<=600) {
-			forward(50);
-		}
-
-		if(600<count) {
-			break;
-		}
+		bool controlFlag = emergency();
+		if(controlFlag == false) ardrone.close();
 
 		ardrone.move3D(vx, vy, vz, vr);
 
-		count++;  
-		printf("count == %d ",count);
+		move_count++;
 
-		// Change camera
-		static int mode = 0;
-		if (key == 'c') ardrone.setCamera(++mode%4);
-
-		// Display the image
-		cvShowImage("camera", image);
-	}
-
-	ardrone.landing();
-
-	// See you
-	ardrone.close();
-
-	return 0;
-} 
-
-
-void roll(int move_count){
-	for(int i=0;i<move_count;i++){
-
-		ardrone.move3D(0,0,0,100.0);
-		printf("roll ");
+		if(move_count==move) break;
 
 	}
 }
 
-void forward(int move_count){
-	for(int i=0;i<move_count;i++){
+void forward(int move){
 
-		ardrone.move3D(400.0,0,0,0);
-		printf("forward ");
+	int move_count = 0;
+
+	while (1) {
+
+		vx = 1.0;
+		vy = 0.0;
+		vz = 0.0;
+		vr = 0.0;
+
+		bool controlFlag = emergency();
+		if(controlFlag == false) ardrone.close();
+
+		ardrone.move3D(vx, vy, vz, vr);
+
+		move_count++;
+
+		if(move_count==move) break;
 
 	}
 }
 
-void stop(int move_count){
-	for(int i=0;i<move_count;i++){
+void stop(int move){
 
-		ardrone.move3D(0.0,0.0,0.0,0.0);
-		printf("stop ");
+	int move_count = 0;
+
+	while (1) {
+
+		vx = 0.0;
+		vy = 0.0;
+		vz = 0.0;
+		vr = 0.0;
+
+		bool controlFlag = emergency();
+		if(controlFlag == false) ardrone.close();
+
+		ardrone.move3D(vx, vy, vz, vr);
+
+		move_count++;
+
+		if(move_count==move) break;
 
 	}
+}
+
+bool emergency() {
+
+	// Key input
+	int key = cvWaitKey(33);
+	if (key == 0x1b) false;
+
+	// Update
+	if (!ardrone.update()) false;
+
+	// Get an image
+	IplImage *image = ardrone.getImage();
+
+	// Take off / Landing 
+	if (key == ' ') {
+		if (ardrone.onGround()) ardrone.takeoff();
+		else                    ardrone.landing();
+	}
+
+	// Move
+
+	if (key == 0x260000) vx =  1.0;
+	if (key == 0x280000) vx = -1.0;
+	if (key == 0x250000) vr =  1.0;
+	if (key == 0x270000) vr = -1.0;
+	if (key == 'q')      vz =  1.0;
+	if (key == 'a')      vz = -1.0;
+
+	
+	// Change camera
+	static int mode = 0;
+	if (key == 'c') ardrone.setCamera(++mode%4);
+
+	// Display the image
+	cvShowImage("camera", image);
+
+	return true;
+
 }
