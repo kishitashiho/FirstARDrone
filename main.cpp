0ca4b2119e8255d89b4dd8f7024e3b8b68a38c05
kishitashiho <sc111016@ibe.kagoshima-u.ac.jp>
2014-04-07 20:19:32 +0900

    赤色を認識しました

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "ardrone/ardrone.h"
+#include "time.h"
 
 // --------------------------------------------------------------------------
 // main(Number of arguments, Argument values)
@@ -8,13 +9,25 @@
 
 ARDrone ardrone;
 
-void roll(int move);
-void forward(int move);
-void stop(int move);
+void roll(int interval_sec);
+void forward(int interval_sec);
+void stop(int interval_sec);
+void rise(int interval_sec);
+void RedChase(void);
+
 void init();
 bool emergency();
 
-double vx = 0.0, vy = 0.0, vz = 0.0, vr = 0.0;
+struct {
+	double vx;
+	double vy;
+	double vz;
+	double vr;
+} ardrone_parameter;
+
+int h, s, v;
+//long int a, b, c;
+IplImage *image = 0;
 
 int main(int argc, char **argv) {
 
@@ -26,17 +39,20 @@ int main(int argc, char **argv) {
 
 	init();
 
-	ardrone.takeoff();
+	//ardrone.takeoff();
 
-	stop(50);
-	forward(100);
-	stop(50);
-	roll(70);
-	stop(50);
-	forward(100);
-	stop(50);
+	RedChase();
 
-	ardrone.landing();
+	//stop(5);
+	//forward(100);
+	//stop(50);
+	//roll(3);
+	//stop(5);
+	//forward(100);
+	//rise(1);
+	//stop(3);
+
+	//ardrone.landing();
 
 	// See you
 	ardrone.close();
@@ -71,71 +87,158 @@ void init() {
 }
 
 
-void roll(int move){
+void roll(int interval_sec){
 
-	int move_count = 0;
+	//int move_count = 0;
+	time_t old_time = time(NULL);
 
 	while (1) {
 
-		vx = 0.0;
-		vy = 0.0;
-		vz = 0.0;
-		vr = 1.0;
+		ardrone_parameter.vx = 0.0;
+		ardrone_parameter.vy = 0.0;
+		ardrone_parameter.vz = 0.0;
+		ardrone_parameter.vr = 1.0;
 
 		bool controlFlag = emergency();
 		if(controlFlag == false) ardrone.close();
 
-		ardrone.move3D(vx, vy, vz, vr);
+		ardrone.move3D(ardrone_parameter.vx, ardrone_parameter.vy, ardrone_parameter.vz, ardrone_parameter.vr);
 
-		move_count++;
+		//move_count++;
+		time_t now_time = time(NULL);
 
-		if(move_count==move) break;
-
+		//if(move_count==move) break;
+		if(now_time - old_time > interval_sec) break;
 	}
 }
 
-void forward(int move){
+void forward(int interval_sec){
 
-	int move_count = 0;
+	//int move_count = 0;
+	time_t old_time = time(NULL);
 
 	while (1) {
 
-		vx = 1.0;
-		vy = 0.0;
-		vz = 0.0;
-		vr = 0.0;
+		ardrone_parameter.vx = 1.0;
+		ardrone_parameter.vy = 0.0;
+		ardrone_parameter.vz = 0.0;
+		ardrone_parameter.vr = 0.0;
 
 		bool controlFlag = emergency();
 		if(controlFlag == false) ardrone.close();
 
-		ardrone.move3D(vx, vy, vz, vr);
+		ardrone.move3D(ardrone_parameter.vx, ardrone_parameter.vy, ardrone_parameter.vz, ardrone_parameter.vr);
 
-		move_count++;
+		//move_count++;
+		time_t now_time = time(NULL);
 
-		if(move_count==move) break;
-
+		//if(move_count==move) break;
+		if(now_time - old_time > interval_sec) break;
 	}
 }
 
-void stop(int move){
+void stop(int interval_sec){
 
-	int move_count = 0;
+	//int move_count = 0;
+	time_t old_time = time(NULL);
 
 	while (1) {
 
-		vx = 0.0;
-		vy = 0.0;
-		vz = 0.0;
-		vr = 0.0;
+		ardrone_parameter.vx = 0.0;
+		ardrone_parameter.vy = 0.0;
+		ardrone_parameter.vz = 0.0;
+		ardrone_parameter.vr = 0.0;
 
 		bool controlFlag = emergency();
 		if(controlFlag == false) ardrone.close();
 
-		ardrone.move3D(vx, vy, vz, vr);
+		ardrone.move3D(ardrone_parameter.vx, ardrone_parameter.vy, ardrone_parameter.vz, ardrone_parameter.vr);
 
-		move_count++;
+		//move_count++;
+		time_t now_time = time(NULL);
 
-		if(move_count==move) break;
+		//if(move_count==move) break;
+		if(now_time - old_time > interval_sec) break;
+	}
+}
+
+void rise(int interval_sec){
+
+	//int move_count = 0;
+	time_t old_time = time(NULL);
+
+	while (1) {
+
+		ardrone_parameter.vx = 0.0;
+		ardrone_parameter.vy = 0.0;
+		ardrone_parameter.vz = 1.0;
+		ardrone_parameter.vr = 0.0;
+
+		bool controlFlag = emergency();
+		if(controlFlag == false) ardrone.close();
+
+		ardrone.move3D(ardrone_parameter.vx, ardrone_parameter.vy, ardrone_parameter.vz, ardrone_parameter.vr);
+
+		//move_count++;
+		time_t now_time = time(NULL);
+
+		//if(move_count==move) break;
+		if(now_time - old_time > interval_sec) break;
+	}
+}
+
+void RedChase() {
+	while(1) {
+
+		image = ardrone.getImage();
+
+		IplImage *red_image = cvCreateImage(cvSize(image->width, image->height), IPL_DEPTH_8U, 3);
+		IplImage *hsv_image = cvCreateImage(cvSize(image->width, image->height), IPL_DEPTH_8U, 3);
+		IplImage *niti = cvCreateImage(cvSize(image->width, image->height), IPL_DEPTH_8U, 3);
+
+		cvCvtColor(image, hsv_image,CV_BGR2HSV);
+		cvSetZero(niti);
+
+		for(int y=0; y<hsv_image->height; y++) {
+			for(int x=0; x<hsv_image->width; x++) {
+				int n = (x + y * hsv_image->width) * 3;
+				h = (unsigned char) * (hsv_image->imageData + n);
+				s= (unsigned char) * (hsv_image->imageData + n + 1);
+				v = (unsigned char) * (hsv_image->imageData + n + 2);
+
+				if(h < 10 || 170 < h) {
+
+					if(90 < s) {
+
+						((unsigned char*)(niti->imageData + niti->widthStep*y))[x*3] = 255;
+						((unsigned char*)(niti->imageData + niti->widthStep*y))[x*3+1] = 255;
+						((unsigned char*)(niti->imageData + niti->widthStep*y))[x*3+2] = 255;
+
+					}
+				}
+			}
+		}
+		
+		cvErode(niti, niti);
+		cvErode(niti, niti);
+		cvErode(niti, niti);
+		cvErode(niti, niti);
+
+		cvDilate(niti, niti);
+		cvDilate(niti, niti);
+		cvDilate(niti, niti);
+		cvDilate(niti, niti);
+
+		cvShowImage("Camera", image);
+		cvShowImage("sub", niti);
+
+		cvReleaseImage(&red_image);
+		cvReleaseImage(&niti);
+		cvReleaseImage(&hsv_image);
+
+
+		bool controlFlag = emergency();
+		if(controlFlag == false) ardrone.close();
 
 	}
 }
@@ -144,10 +247,10 @@ bool emergency() {
 
 	// Key input
 	int key = cvWaitKey(33);
-	if (key == 0x1b) false;
+	if (key == 0x1b) return false;
 
 	// Update
-	if (!ardrone.update()) false;
+	if (!ardrone.update()) return false;
 
 	// Get an image
 	IplImage *image = ardrone.getImage();
@@ -160,12 +263,12 @@ bool emergency() {
 
 	// Move
 
-	if (key == 0x260000) vx =  1.0;
-	if (key == 0x280000) vx = -1.0;
-	if (key == 0x250000) vr =  1.0;
-	if (key == 0x270000) vr = -1.0;
-	if (key == 'q')      vz =  1.0;
-	if (key == 'a')      vz = -1.0;
+	if (key == 0x260000) ardrone_parameter.vx =  1.0;
+	if (key == 0x280000) ardrone_parameter.vx = -1.0;
+	if (key == 0x250000) ardrone_parameter.vr =  1.0;
+	if (key == 0x270000) ardrone_parameter.vr = -1.0;
+	if (key == 'q')      ardrone_parameter.vz =  1.0;
+	if (key == 'a')      ardrone_parameter.vz = -1.0;
 
 	
 	// Change camera
